80cbcd2d8109e26b459d1a1a2f1bf6b16d13cd09
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-05-01 19:16:26 +0530

    created some other pages

---
+++ src/pages/GlobalReach.jsx
@@ -0,0 +1,8 @@
+const GlobalReach=()=>{
+    return(
+        <div>
+            <h4>Globalreach Component</h4>
+        </div>
+    )
+}
+export default GlobalReach;
---
+++ src/pages/Services.jsx
@@ -0,0 +1,8 @@
+const Services=()=>{
+    return(
+        <div>
+            <h4>Services Component</h4>
+        </div>
+    )
+}
+export default Services;
