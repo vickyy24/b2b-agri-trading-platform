bc0a7d04ab89799069871829c09624dfd0554082
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-05-12 21:59:12 +0530

    Fixed admin tab active state using route-based navigation

--- frontend/src/pages/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel.jsx
@@ -1,17 +1,10 @@
-import { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+// import { useState } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const Admin = () => {
 
-    const [activeTab, setActiveTab] = useState("category");
+    const location = useLocation();
 
-    function handleCategoryTab(){
-        setActiveTab("category");
-    }
-
-    function handleProductTab(){
-        setActiveTab("product");
-    }
     return (
         <div className="min-h-screen bg-[#f8faf7] px-6 py-6">
 
@@ -22,13 +15,13 @@ const Admin = () => {
 
             <div className="flex gap-4 mb-5">
 
-                <Link to="category" onClick={handleCategoryTab} className={
-                    activeTab === "category" ? "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-[#2f855a] text-white " : "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-white border border-gray-300 text-gray-700 "}>
+                <Link to="category" className={
+                    location.pathname === "/admin/category" ? "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-[#2f855a] text-white " : "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-white border border-gray-300 text-gray-700"}>
                     Categories
                 </Link>
 
-               <Link to="product" onClick={handleProductTab} className={
-                    activeTab === "product" ? "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-[#2f855a] text-white " : "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-white border border-gray-300 text-gray-700 "}>
+               <Link to="product" className={
+                    location.pathname === "/admin/product" ? "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-[#2f855a] text-white " : "px-5 py-2.5 rounded-lg font-semibold cursor-pointer bg-white border border-gray-300 text-gray-700"}>
                     Products
                 </Link>
 
