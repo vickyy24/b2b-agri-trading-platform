bc11bb7fcecf67895b9677ed9218f194f1a37389
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-05-30 22:26:17 +0530

    redux store added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import "./main.css";
 import App from './App';
 
+import { Provider } from "react-redux";
+import Store from "./Redux/store";
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <StrictMode>
+
+        <Provider store={Store}>
+
+            <App />
+
+        </Provider>
+
+    </StrictMode>
+
 )
