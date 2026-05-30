bb549622de314c38f3142f6c0ec3d32898210ecd
Vikas Sontakke <vikassontakke2002@gmail.com>
2026-05-30 22:31:18 +0530

    navigated cart icon at navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,7 +1,20 @@
 import { NavLink } from "react-router-dom"
 import logo from "../assets/PadmayogLogo.png"
+import { useSelector } from "react-redux";
+import { PiShoppingCart } from "react-icons/pi";
 
 const Navbar=()=>{
+
+    const cartitems = useSelector((state)=>
+        state.cart.cartItems
+    )
+
+    let itemCount = 0;
+    cartitems.forEach((item)=>{
+        itemCount += item.quantity;
+    })
+
+
     return(
         <nav className="w-full absolute  z-50 border-b border-white/30">
             <div className="flex justify-between items-center px-4 py-2 ">
@@ -11,7 +24,7 @@ const Navbar=()=>{
                     
                 </NavLink>
 
-                <ul className="flex gap-6 text-white font-medium">
+                <ul className="flex gap-6 items-center text-white font-medium">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/commodities">Commodities</NavLink></li>
@@ -19,6 +32,21 @@ const Navbar=()=>{
                     <li><NavLink to="/trade-process">Trade Process</NavLink></li>
                     <li><NavLink to="/global-reach">Global Reach</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
+                    <li>
+                        <NavLink to="/cart" className="relative flex items-center gap-2 bg-[#8b4513] hover:bg-[#622e09] px-4 py-2 rounded-lg">
+
+                            <p className="absolute top-0 left-7 bg-red-600 min-w-5 h-5 text-xs rounded-full flex justify-center items-center">
+                                {itemCount}
+                            </p>
+
+                            <PiShoppingCart className="text-3xl"/>
+
+                            <p>
+                                Cart
+                            </p>
+
+                        </NavLink>
+                    </li>
                 </ul>
             </div>
         </nav>
